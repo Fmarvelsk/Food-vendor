3967f801fde3414875f6caef1ee5330d64e33f92
fmarvelsk <fmarvelsk@gmail.com>
2020-12-11 10:19:30 +0100

    removed

--- src/Components/CarouselFood.js
+++ src/Components/CarouselFood.js
@@ -15,15 +15,7 @@ const CarouselFood = () => {
             <img class="d-block w-100" src={Group2} alt="First slide"/>
            </div>
         </div>
-        <a class="carousel-control-prev" href="#my-carousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#my-carousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+     </div>
     
     )
 }
