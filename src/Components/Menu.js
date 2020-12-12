11ba969d58e6e3b4fb7c30c25854ea5cb86a0f43
fmarvelsk <fmarvelsk@gmail.com>
2020-12-12 12:42:20 +0100

    this works

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,10 +1,17 @@
-import { Container, Row } from 'react-bootstrap'
+import { Container, Col, Row, Form } from 'react-bootstrap'
 import Group1 from '../Image/Group 113.png'
 
 const Menu = () => {
     return(
         <Container className="jumbotron jumbocolor">
-            <Row className="content">
+            <Row>
+            <Col md={2}  className="content">
+                <div className="checkbox-menu pt-3">
+                <Form.Check /> <p>Soup</p>
+                </div>
+            </Col>
+            <Col md={8}>
+            </Col>
             
             </Row>
         </Container>
