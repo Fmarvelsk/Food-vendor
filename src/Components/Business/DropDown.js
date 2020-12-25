f9f82eae559dd20aedfea5db56e82f110c1b81ee
fmarvelsk <fmarvelsk@gmail.com>
2020-12-25 15:11:15 +0100

    categories

--- src/Components/Business/DropDown.js
+++ src/Components/Business/DropDown.js
@@ -1,23 +1,37 @@
 import { Dropdown } from 'react-bootstrap'
-const DropDown = (props) => {
+const DropDown = () => {
     return(
-        <div>
-              <Dropdown className="search-m">
-                      <Dropdown.Toggle id="dropdown-basic">
-                         {props.item.Type} 
-                      </Dropdown.Toggle>
-                    {console.log(props)}
-            {props.item?.item.map( (res, i) => (
-                      <Dropdown.Menu>
-                          <Dropdown.Item >
-                              {console.log(res.title)}
-                          </Dropdown.Item>
-                                </Dropdown.Menu>
-                   
-    
-            ))}
-            </Dropdown>
-                    </div>
+        <div >
+          <Dropdown className="search-m">
+							<Dropdown.Toggle id="dropdown-basic">
+								Catergories: 
+							</Dropdown.Toggle>
+							<Dropdown.Menu>
+								<Dropdown.Item >
+									Soup
+								</Dropdown.Item>
+								<Dropdown.Item >
+									Swallow
+								</Dropdown.Item>
+								<Dropdown.Item >
+									Rice
+								</Dropdown.Item>
+                                <Dropdown.Item >
+									Slides
+								</Dropdown.Item>
+                                <Dropdown.Item >
+									Breads
+								</Dropdown.Item>
+                                <Dropdown.Item >
+									Beans
+								</Dropdown.Item>
+                                <Dropdown.Item >
+                                    Others
+								</Dropdown.Item>
+							
+							</Dropdown.Menu>
+						</Dropdown> 
+          </div>
           
     )
 }
